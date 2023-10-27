9d4933cbce2a2bcf5789c9e65972c67b232adcf9
Francisco Ferreira <ferreiradeveloper@gmail.com>
2023-10-27 23:35:57 +0200

    terminada update del cliente con ruta dinamica

--- controllers/client.ts
+++ controllers/client.ts
@@ -14,10 +14,36 @@ export const getAll = async (req: any, res: Response) => {
     }    
 }
 
-export const create = async (req: any, res: Response) => {
-    console.log({ body: req.body })
-    const cretedClient = await ClientModel.create( req.body );
-    res.status(201).json( { ok: true, data: cretedClient } );
+export const getById = async (req: any, res: Response) => {
+    const { id } = req.params
+    try {
+
+        const client = await ClientModel.findById(id);
+
+        res.status(200).json({ ok: true , data: client });        
+    } catch (error) {
+        res.status(500).json({ok: false, message: "Error del servidor"})
+    }    
 }
 
+export const create = async (req: any, res: Response) => {
+    console.log({ body: req.body })
+    try {
+        const cretedClient = await ClientModel.create( req.body );
+        res.status(201).json( { ok: true, data: cretedClient } );
+        
+    } catch (error) {
+        res.status(500).json({ok: false, message: "Error del servidor"})
+    }
+}
 
+export const update = async (req: any, res: Response) => {
+    const {id} = req.params;
+    try {
+        const updatedClient = await ClientModel.findByIdAndUpdate( id, req.body );
+        res.status(201).json( { ok: true, data: updatedClient } );    
+    } catch (error) {
+        res.status(500).json({ok: false, message: "Error del servidor"})
+    }
+    
+}
--- routes/clients.ts
+++ routes/clients.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getAll, create } from '../controllers/client';
+import { getAll, create, getById, update } from '../controllers/client';
 import { validateuser } from '../middlewares/auth';
 
 
@@ -8,7 +8,9 @@ const router = express.Router();
 router.use(validateuser());
 
 router.get('/', getAll);
+router.get('/:id', getById);
 router.post('/', create);
+router.put("/:id", update)
 
 export default router;
 
